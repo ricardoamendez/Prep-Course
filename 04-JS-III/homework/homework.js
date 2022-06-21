52d044a41ed7f30c2c611de9e30b19af31edc599
ricardoamendez <ricardomendezg23@gmail.com>
2022-06-20 20:49:11 -0500

    Homework HTML complete

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,21 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return array [0]
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array[array.length -1]
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length;
 }
 
 
@@ -23,6 +26,11 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  var matriz = [];
+  for(var i = 0; i < array.length; i++) {
+    matriz[i] = array[i] + 1;
+  }
+  return matriz;
 }
 
 
